f887a694887d5ac24c3ff90fd9e09056f1b09c85
emily5 <milleremilly5@gmail.com>
2024-09-18 11:50:28 -0300

    :tada: Commit inicial

---
+++ aula02/Caneta.java
@@ -0,0 +1,31 @@
+package aula02;
+
+public class Caneta {
+    String modelo;
+    String cor;
+    float ponta;
+    int carga;
+    boolean tampada;
+    void status() {
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Uma caneta " + this.cor); //quem chamou o método status, vai ser substituído por this
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+    void rabiscar() {
+        if (this.tampada == true) {
+            System.out.println("ERRO! Não posso rabiscra");
+        } else {
+            System.out.println("Estou Rabiscando");
+        }
+    }
+
+    void tampar() {
+            this.tampada = true;
+    }
+
+    void destampar() {
+        this.tampada = false;
+    }
+}
